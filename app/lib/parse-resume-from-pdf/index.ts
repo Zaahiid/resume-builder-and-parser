01afdf3e4ef8795bdf2839a829a85f542b45e663
Zaahiid <zahidrsheikh01@gmail.com>
2024-08-05 00:13:36 +0530

    Added Functions for parsing of the PDF 📕

---
+++ app/lib/parse-resume-from-pdf/index.ts
@@ -0,0 +1,16 @@
+import { groupLinesIntoSections } from "./group-lines-into-sections";
+import { groupTextItemsIntoLines } from "./group-text-items-into-lines";
+import { readPdf } from "./read-pdf";
+
+export const parseResumeFromPdf = async (fileUrl: string) => {
+  //step 1. Read a pdf resume file into text items to prepare for processing
+  const textItems = await readPdf(fileUrl);
+
+  //step 2. Group text items into lines
+  const lines = groupTextItemsIntoLines(textItems);
+
+  //step 3. Group lines into sections
+  const sections = groupLinesIntoSections(lines);
+
+  return;
+};
